6ef1da9d80688e4f35b2ff235ba85f768cbeb1dd
Peter Duff <peterduff@gmail.com>
2021-04-06 15:37:50 +0100

    RMPI-13 Tooltips included

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { HeaderInterceptor } from './interceptors/header.interceptor';
-import { NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
-import { SnomedNavbarComponent } from './components/snomed-navbar/snomed-navbar.component';
-import { SnomedFooterComponent } from './components/snomed-footer/snomed-footer.component';
-import { AuthenticationService } from './services/authentication/authentication.service';
-import { AuthoringService } from './services/authoring/authoring.service';
-import { RequestTabsComponent } from './components/request-tabs/request-tabs.component';
-import { AddConceptFormComponent } from './components/add-concept-form/add-concept-form.component';
-import { AddDescriptionFormComponent } from './components/add-description-form/add-description-form.component';
-import { AddRelationshipFormComponent } from './components/add-relationship-form/add-relationship-form.component';
-import { ChangeDescriptionFormComponent } from './components/change-description-form/change-description-form.component';
-import { ChangeRelationshipFormComponent } from './components/change-relationship-form/change-relationship-form.component';
-import { InactivateDescriptionFormComponent } from './components/inactivate-description-form/inactivate-description-form.component';
-import { InactivateRelationshipFormComponent } from './components/inactivate-relationship-form/inactivate-relationship-form.component';
-import { OtherFormComponent } from './components/other-form/other-form.component';
-import { MatRadioModule } from '@angular/material/radio';
-import { ToastrModule } from 'ngx-toastr';
-import { AuthBlockComponent } from './components/auth-block/auth-block.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {HeaderInterceptor} from './interceptors/header.interceptor';
+import {NgbTypeaheadModule} from '@ng-bootstrap/ng-bootstrap';
+import {SnomedNavbarComponent} from './components/snomed-navbar/snomed-navbar.component';
+import {SnomedFooterComponent} from './components/snomed-footer/snomed-footer.component';
+import {AuthenticationService} from './services/authentication/authentication.service';
+import {AuthoringService} from './services/authoring/authoring.service';
+import {RequestTabsComponent} from './components/request-tabs/request-tabs.component';
+import {AddConceptFormComponent} from './components/add-concept-form/add-concept-form.component';
+import {AddDescriptionFormComponent} from './components/add-description-form/add-description-form.component';
+import {AddRelationshipFormComponent} from './components/add-relationship-form/add-relationship-form.component';
+import {ChangeDescriptionFormComponent} from './components/change-description-form/change-description-form.component';
+import {ChangeRelationshipFormComponent} from './components/change-relationship-form/change-relationship-form.component';
+import {InactivateDescriptionFormComponent} from './components/inactivate-description-form/inactivate-description-form.component';
+import {InactivateRelationshipFormComponent} from './components/inactivate-relationship-form/inactivate-relationship-form.component';
+import {OtherFormComponent} from './components/other-form/other-form.component';
+import {MatRadioModule} from '@angular/material/radio';
+import {ToastrModule} from 'ngx-toastr';
+import {AuthBlockComponent} from './components/auth-block/auth-block.component';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @NgModule({
     declarations: [
@@ -47,6 +48,7 @@ import { AuthBlockComponent } from './components/auth-block/auth-block.component
         BrowserAnimationsModule,
         NgbTypeaheadModule,
         MatRadioModule,
+        MatTooltipModule,
         ToastrModule.forRoot(),
         ReactiveFormsModule
     ],
