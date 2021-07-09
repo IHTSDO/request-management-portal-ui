c904f2a9117edbb3316c6da10df92e1167d7ccb6
Peter Duff <peterduff@gmail.com>
2021-07-09 11:10:32 +0100

    RMPI-38 Updated dashboard config

--- concatenate.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs-extra');
-
-concatenate = async () =>{
-    await fs.ensureDir('dist/request-management-portal-ui');
-    await fs.copy('./src/dashboard/', 'dist/request-management-portal-ui/dashboard/');
-}
-concatenate();
