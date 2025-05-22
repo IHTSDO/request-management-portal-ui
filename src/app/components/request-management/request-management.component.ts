442ad98583a0c1b97b7711e4cac3dfbe6fcead5b
Peter Duff <peterduff@gmail.com>
2025-05-22 09:50:26 +0100

    RMPI-84 UI done for management page

---
+++ src/app/components/request-management/request-management.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-request-management',
+  imports: [RouterLink],
+  templateUrl: './request-management.component.html',
+  styleUrl: './request-management.component.scss'
+})
+export class RequestManagementComponent {
+
+}
