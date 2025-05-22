7c9a978a333dbc73b1c4214edabc345a9905db65
Peter Duff <peterduff@gmail.com>
2025-05-22 09:50:43 +0100

    RMPI-83 UI started for form page

---
+++ src/app/components/request/request.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-request',
+    imports: [],
+  templateUrl: './request.component.html',
+  styleUrl: './request.component.scss'
+})
+export class RequestComponent {
+
+}
