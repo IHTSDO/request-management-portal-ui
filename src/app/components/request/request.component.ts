fff0dada7208a3f30535e367c7b486d35a30e8c3
Peter Duff <peterduff@gmail.com>
2025-05-23 11:14:31 +0100

    RMPI-83 Form page updated

--- src/app/components/request/request.component.ts
+++ src/app/components/request/request.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
+import {Component} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {NgIf} from '@angular/common';
 
 @Component({
-  selector: 'app-request',
-    imports: [],
-  templateUrl: './request.component.html',
-  styleUrl: './request.component.scss'
+    selector: 'app-request',
+    imports: [FormsModule, NgIf],
+    templateUrl: './request.component.html',
+    styleUrl: './request.component.scss'
 })
 export class RequestComponent {
-
+    public formType: string = 'add-concept';
 }
